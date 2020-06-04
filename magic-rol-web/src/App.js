068281de29241daa6ab276624b96881d54655e95
Gaston Tomas Huete <44206933+GastonTH@users.noreply.github.com>
2020-06-04 23:02:58 +0200

    buscando opciones a la locura

--- magic-rol-web/src/App.js
+++ magic-rol-web/src/App.js
@@ -2,32 +2,72 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
 export default class App extends Component {
 
-  constructor(props) {
+  constructor(props){
 
     super(props)
     this.state = { 
+      /*
+      - 0 Inicio
+      - 1 Loggin
+      - 2 Mi perfil
+      - 3 Fichas de ejemplo
+      - 4 Salas
+      */
       selector_menu : 0,
+
       loggin : false // estado para saber si estas logeado
      };
 
-    this.cambioPestanya = this.cambioPestanya.bind(this);/* importante blindar el contenido de una funcion para que pueda acceder al state */
-}
+    /* importante blindar el contenido de una funcion para que pueda acceder al state */
+    this.cambioPestanya = this.cambioPestanya.bind(this);
+    this.llamarEleccion = this.llamarEleccion.bind(this);
+  }
 
-cambioPestanya(){
-  this.setState({selector_menu: 1 })
-}
+  llamarEleccion(){
+
+    switch (this.state.selector_menu) {
+      case 0:
+      return(<div>Inicio</div>)      
+
+      case 1: 
+      return(<div>Fichas</div>)
+    }
+
+
+  }
+
+  cambioPestanya(n){
+    console.log(n);
+  }
+
+  llamadaMenu(){
+    return(
+
+      <div className='menuTop'>
+        <ul>
+            <li><img href='./img/icono.png' alt='icono web'/></li>
+            <li>Inicio</li>
+            <li onClick={this.cambioPestanya(1)}>Fichas de ejemplo</li>
+            <li>Login</li>
+            <li>Salas</li>            
+        </ul>
+        </div>
+
+    )
+  }
+
+
 
 render(){
 
-  return(
-
-    <div className=''></div>
-
-  )
-
-}
+    return(
+      <div>
+        {this.llamadaMenu()}
+        {this.llamarEleccion()}
+      </div>
+    )
+} 
 
 }
---
+++ magic-rol-web/src/components/Loggin/Loggin.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Loggin.css';
+
+
+export default class Loggin extends Component {
+
+    render(){
+
+        return(
+
+            <div>Loggin</div>
+
+        )
+
+    }
+
+}
