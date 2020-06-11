a3156c6420bb2eef18b5d2c7c85c5d08efd6aa7d
Gaston Tomas Huete <44206933+GastonTH@users.noreply.github.com>
2020-06-11 13:28:43 +0200

    conexion satisfactoria y sin errores

---
+++ secondTry/src/database.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql');
+const { database } = require('./db.config');
+const { promisify } = require('util');
+//creando la conexion
+
+const connection = mysql.createPool(database);
+
+connection.getConnection((err, connection) => {
+
+    if (err) {
+
+        if (err.code == 'PROTOCOL_CONNECTION_LOST') { //si la base de datos pierde la conexion
+            console.error('ERROR LA CONEXION A LA BASE DE DATOS SE CERRO');
+        }
+
+        if (err.code == 'ER_CON_COUNT_ERROR') {
+            console.error('LA BASE DE DATOS TIENE DEMASIADAS CONEXIONES')
+        }
+
+        if (err.code == 'ECONNREFUSED') {
+            console.error('LA CONEXION FUE RECHAZADA');
+        }
+
+    }
+
+    if (connection) connection.release();
+    console.log('CONEXION SATISFACTORIA');
+    return;
+});
+
+connection.query = promisify(connection.query); //forma de hacer promesas con node y su complemento promisify de utils
+
+module.exports = connection;
--- secondTry/src/routes/links.js
+++ secondTry/src/routes/links.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
+const connection = require('../database');
+
 module.exports = router;
